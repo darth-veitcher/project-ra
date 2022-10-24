1f335db3ff0f19ef051ef6235f8b019c95cc4e99
Darth Veitcher <darth-veitcher@statemachine.ai>
2022-10-24 12:24:56 +0100

    add ci pipeline

--- docs/index.md
+++ docs/index.md
@@ -5,6 +5,8 @@ hide:
 #   - toc
 ---
 
+[![status-badge](https://ci.jamesveitch.xyz/api/badges/james/project-ra/status.svg)](https://ci.jamesveitch.xyz/james/project-ra)
+
 This repository is designed to document my approach to setting up a basic Home Assistant installation with a connection to Victron Energy devices (solar mppt charge controller, battery smartshunt, inverter etc.)
 
 Many of the settings will require changing for your local configuration so **please read the documentation** before attempting to blindly run a `docker compose up -d`.
---
+++ noxfile.py
@@ -0,0 +1,49 @@
+"""Configuration file for Nox."""
+import tempfile
+import nox  # noqa: I003
+from nox import Session
+
+
+def install_with_constraints(session: Session, *args, **kwargs):
+    """Installs packages based on poetry lockfile. Acts as a wrapper for session.install()."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--without-hashes",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session,
+        "mkdocs",
+        "mkdocs-material",
+        "mkdocs-minify-plugin",
+        "pymdown-extensions",
+        "mike",
+    )
+    session.run("make", "docs-dev", external=True)
+
+
+@nox.session
+def docs_production(session: Session) -> None:
+    """Build the documentation."""
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session,
+        "mkdocs",
+        "mkdocs-material",
+        "mkdocs-minify-plugin",
+        "pymdown-extensions",
+        "mike",
+    )
+    session.run("make", "docs-prod", external=True)
